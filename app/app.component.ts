288ecb8e844506f617e4989d494a5a2f36a5df8f
Milton Becker Junior <miltonbecker@gmail.com>
2016-07-18 00:18:32 -0300

    Third level: refactoring, model and mock

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CarPartsComponent } from './car-parts.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <car-parts></car-parts>
+    `,
+  directives: [CarPartsComponent]
+})
+export class AppComponent {
+  title = 'Ultra Racing';
+}
---
+++ app/car-parts.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CarPart } from './car-part';
+import { CARPARTS } from './mocks';
+
+@Component({
+  selector: 'car-parts',
+  templateUrl: 'app/car-parts.component.html',
+  styleUrls: ['app/car-parts.component.css']
+})
+export class CarPartsComponent {
+  carParts: CarPart[];
+
+  ngOnInit() {
+    this.carParts = CARPARTS;
+  }
+
+  totalCarParts() {
+    let sum = 0;
+
+    for (let carPart of this.carParts) {
+      sum += carPart.inStock;
+    }
+
+    return sum;
+  }
+}
